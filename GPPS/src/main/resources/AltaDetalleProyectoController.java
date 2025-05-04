4a1b78be7ac861de127706f2b85867aae6a4f31b
mariabaselli <mariangelesbaselli@hotmail.com>
2025-05-04 16:58:25 -0300

    Traer carpeta frontend desde develop

---
+++ GPPS/src/main/resources/AltaDetalleProyectoController.java
@@ -0,0 +1,44 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class AltaDetalleProyectoController {
+
+    @FXML
+    private Button aceptarDetalleProyectoBoton;
+
+    @FXML
+    private TextArea aptitudesProyectoAreaTexto;
+
+    @FXML
+    private TextArea areasInteresAreaDeTexto;
+
+    @FXML
+    private Button cancelarDetallePryoyectoBoton;
+
+    @FXML
+    private TextArea descripcionAreaDeTexto;
+
+    @FXML
+    private TextArea objetivosProyectoAreaTexto;
+
+    @FXML
+    private TextField tituloCampoDeTexto;
+
+    @FXML
+    private Label tituloDelProyecto;
+
+    @FXML
+    void botonAceptarPresionado(ActionEvent event) {
+
+    }
+
+    @FXML
+    void botonCancelarPresionado(ActionEvent event) {
+
+    }
+
+}
