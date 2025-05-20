00ffe62eea812f84f5af768b370d3234ad86ab2b
DanielAntrichipay <danielantrichipay20@gmail.com>
2025-05-20 20:16:25 -0300

    Cree las interfaces que se desacoplan las capas.

---
+++ GPPS/src/main/java/Modelo/Modelo.java
@@ -0,0 +1,6 @@
+package Modelo;
+
+public class Modelo implements ModeloInterfaz{
+    private PersistenciaInterfaz persistencia;
+
+}
---
+++ GPPS/src/main/java/Persistencia/Persistencia.java
@@ -0,0 +1,6 @@
+package Persistencia;
+
+import Modelo.PersistenciaInterfaz;
+
+public class Persistencia implements PersistenciaInterfaz {
+}
